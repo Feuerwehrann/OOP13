895c5bfd2ec489ee73e1ebfe119f6948d53549d1
Feuerwehrann <94531981+Feuerwehrann@users.noreply.github.com>
2023-10-26 06:40:55 +0000

    added

---
+++ b1_Listen/ArraySample.java
@@ -0,0 +1,22 @@
+package b1_Listen;
+
+public class ArraySample {
+    public static void main(String[] args) {
+        String namen[] = { "Jakob", "Michael", "Helene" };
+        for (int i = 0; i < namen.length; i++) {
+            System.out.println(namen[i]);
+        }
+        System.out.println("---");
+        namen[0] = null;
+        for (int i = 0; i < namen.length; i++) {
+            System.out.println(namen[i]);
+        }
+        System.out.println("---");
+        for (String strNamen : namen) {
+            if (strNamen == "Helene") {
+                System.out.println("Helene vorhanden! Yayyyy!");
+
+            }
+        }
+    }
+}
