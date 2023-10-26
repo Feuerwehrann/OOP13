881343f929fb98a62c687239cc617b01564a10a3
Feuerwehrann <94531981+Feuerwehrann@users.noreply.github.com>
2023-10-26 06:06:46 +0000

    commit

---
+++ b1_Array/Array.java
@@ -0,0 +1,19 @@
+package b1_Array;
+
+public class Array {
+    public static void main(String[] args) {
+        String namen [] = {"Jakob", "Michael", "Helene"};
+        for (int i = 0; i<namen.length; i++){
+            System.out.println(namen[i]);
+        }
+        namen[0]=null;
+        for (int i = 0; i<namen.length; i++){
+            System.out.println(namen[i]);
+        }
+
+
+    }
+
+    
+    
+}
