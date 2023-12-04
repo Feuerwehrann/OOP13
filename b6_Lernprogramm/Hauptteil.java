98298c04d1552190490f923cbe5dd8e53f365c02
Feuerwehrann <94531981+Feuerwehrann@users.noreply.github.com>
2023-12-04 11:59:07 +0000

    added

--- b6_Lernprogramm/Hauptteil.java
+++ b6_Lernprogramm/Hauptteil.java
@@ -1,48 +1,53 @@
 package b6_Lernprogramm;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Hauptteil {
 
     public static void main(String[] args) {
-        String beleidigung [] = {"Du Opfer!", "Du kannst gar nichts!", "Gewitterziege", "Lackaffe", "Holzkopf!", "Schafshirn!", "Hochgeschissner!"}; 
-        String lob [] = {"Du machst das super!", "Du hast das ganz toll gemacht!", "Ich bin stolz auf dich!"};
+        String beleidigung[] = { "Du Opfer!", "Du kannst gar nichts!", "Gewitterziege", "Lackaffe", "Holzkopf!",
+                "Schafshirn!", "Hochgeschissner!" };
+        String lob[] = { "Du machst das super!", "Du hast das ganz toll gemacht!", "Ich bin stolz auf dich!" };
         String frageAktuell = null;
         String antwortAktuell = null;
-         String antwort= null;
-         Scanner sc = new Scanner(System.in); 
-         Map<String, String> ungelernt = new HashMap<String, String>();
+        String antwort = null;
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> halbgelernt = new HashMap<String, String>();
+        Map<String, String> ungelernt = new HashMap<String, String>();
         ungelernt.put("Welche Farbe hat ein Elefant", "Grau");
         ungelernt.put("Wann schneit es?", "Winter");
         ungelernt.put("Wo gibt es die Besten Snacks?", "dm");
-        for(int i = 0; i<=4; i++){
+        for (int i = 0; i <= 4; i++) {
 
-        
-        for (String key: ungelernt.keySet()) {
-            System.out.println("\33[H\033[2J");
-            System.out.flush();
-            System.out.println(key);
-            frageAktuell=key;
-            antwortAktuell= ungelernt.get(frageAktuell);
-            break;
-         }
-        antwort = sc.nextLine();
-        if (antwort.equals(ungelernt.get(frageAktuell))) 
-        {
+            for (String key : ungelernt.keySet()) {
+                System.out.println("\33[H\033[2J");
+                System.out.flush();
+                System.out.println(key);
+                frageAktuell = key;
+                antwortAktuell = ungelernt.get(frageAktuell);
+                break;
+            }
+            antwort = sc.nextLine();
+            if (antwort.equals(ungelernt.get(frageAktuell))) {
 
-            System.out.println(lob[(int)(Math.random() * (4))]);
-            
+                System.out.println(lob[(int) (Math.random() * (4))]);
+                ungelernt.remove(frageAktuell);
+                halbgelernt.put(frageAktuell, antwortAktuell);
+
+            } else {
+                System.out.println(beleidigung[(int) (Math.random() * (8))]);
+                System.out.println("Richtige Antwort: " + ungelernt.get(frageAktuell));
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+
         }
-        else{
-            System.out.println(beleidigung[(int)(Math.random() * (8))]);
-            System.out.println("Richtige Antwort: "+ungelernt.get(frageAktuell));
-        }
-        ungelernt.remove(frageAktuell);
-        ungelernt.put(frageAktuell, antwortAktuell);
         sc.close();
-        
-
     }
-}
 }
