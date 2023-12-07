eaaf2546a8abdb346ea2381fd64f22829f8c180a
Feuerwehrann <94531981+Feuerwehrann@users.noreply.github.com>
2023-12-07 08:25:11 +0000

    added

---
+++ c1_KNN/Main.java
@@ -0,0 +1,64 @@
+package c1_KNN;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static int k = 5;
+    public static void main(String[] args) {
+        ArrayList<DataObject> objs = new ArrayList<DataObject>();
+        objs.add(new DataObject(5,1,0));
+        objs.add(new DataObject(6,1,0));
+        objs.add(new DataObject(5,2,0));
+        objs.add(new DataObject(6,2,0));
+        objs.add(new DataObject(7,1,0));
+        objs.add(new DataObject(0,4,1));
+        objs.add(new DataObject(1,4,1));
+        objs.add(new DataObject(0,5,1));
+        objs.add(new DataObject(1,5,1));
+        DataObject o = new DataObject(2,3);
+        System.out.println(kNN(o, objs));
+
+    }        
+    
+    public static int kNN(DataObject o, ArrayList<DataObject> objs){
+        int [] indices = new int [k];
+        double [] mins = new double[k];
+        for (int i = 0; i<k; i++){
+            mins [i] = Double.MAX_VALUE;
+        }
+        for (int i = 0; i<objs.size();i++){
+            double dist = distance(o, objs.get(i));
+            double max = Double.MIN_VALUE;
+            int maxIdx = 0;
+            for (int j =0; j<k;j++){
+                if(max<mins[i]){
+                    max = mins[j];
+                    maxIdx = j;
+                }
+            }
+            if (mins[maxIdx]>dist){
+                mins[maxIdx] = dist;
+                indices[maxIdx]=i;
+            }
+        }
+        int a =0;
+        int b=0;
+        for (int i = 0; i < k; i++) {
+            if (objs.get(indices[i]).klasse == 0){
+                a++;
+            }
+            else{
+                b++;
+            }
+        }
+        if (a>b){
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+    private static double distance(DataObject o, DataObject ref) {
+        return Math.sqrt((o.x-ref.x)*(o.x-ref.x))+((o.y-ref.y)*o.y-ref.y);
+    }    
+}
