ce7ea7353a065c7bb5951f7db529dcd2a7af048b
Feuerwehrann <94531981+Feuerwehrann@users.noreply.github.com>
2024-02-01 09:26:26 +0100

    Update DataPanel.java

--- KMeansGUI/GUIKMeans/src/KMeansGUI/DataPanel.java
+++ KMeansGUI/GUIKMeans/src/KMeansGUI/DataPanel.java
@@ -1,34 +1,49 @@
-package KMeansGUI;
+package GUIKMeans;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JPanel;
 
-public class DataPanel extends JPanel{
+public class JDataPanel extends JPanel {
 	
-	
-	List<Datapoint> datenpunkte = new ArrayList<>();
-	
-	Graphics myGraphics;
-	
+	ArrayList <Cluster> dieCluster = new ArrayList <Cluster>();
+	@Override
 	protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        myGraphics = g;
-        
-        for (Datapoint dataPoint : datenpunkte) {
-			g.fillOval(dataPoint.x,dataPoint.y,10,10);
-		}
-        //g.drawLine(0, 0, 100, 100);
-        addDataPoint(200,200);
-}
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		for (Cluster c :dieCluster ) {
+			g.setColor(c.getColor());
+			g.fillRect(c.getX(), c.getY(), 10, 10);
+			for (Datapoint d : c.DatapointList) {
+			      g.fillOval(d.getX(), d.getY(), 10, 10);
+			}
 
-	public void addDataPoint(int x, int y) {
+		      
+		    }
+	}
+
+	public void addDataPoint(int x, int y, Color darkGray) {
+		System.out.println("X: "+x+" y: "+y+" added");
+		if (dieCluster.isEmpty()) {
+			dieCluster.add(new Cluster(100, 100, darkGray));
+			
+		}
 		
-		Cluster.add(new Datapoint(x,y));
+		dieCluster.get(0).hinzufuegen(x, y);
+	
+		repaint();
 		
 	}
+
+	public void addCluster(int x, int y, Color co) {
+		System.out.println("X: "+x+" y: "+y+" added, Cluster");
+		dieCluster.add(new Cluster(x,y,co));
+		repaint();
+		
+	}
+
+
 }
